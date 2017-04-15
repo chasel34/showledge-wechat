f06e1f11421e2b4abc8684791276b1c0118631d7
zhongting <1102365435@qq.com>
2017-04-15 21:46:01 +0800

    change something

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,9 +1,48 @@
 Page({
   data: {
-    imgUrls: [
-        '../../common/image/lunbo1.jpg',
-        '../../common/image/lunbo2.jpg',
-        '../../common/image/lunbo3.jpg'
-        ]
+    home: {},  
+    swiperImg: [],
+    article: [],
+    articleDisplay: []
+  },
+  onLoad: function() {
+    this.fetchData()
+  },
+  displayArticle: function() {
+    let that = this
+    let length = this.data.articleDisplay.length
+    for(let i = length; i < length + 20; i++) {
+      this.setData({
+        'articleDisplay[1]': this.data.article[i] 
+      })
     }
+    console.log(param)
+    console.log(this.data.param)
+  },
+  fetchData: function() {
+    let that = this
+    wx.request({
+      url: 'http://127.0.0.1:8080/api/home',
+      success: function(res) {
+        that.setData({
+          home: res.data.data
+        })
+        that.setData({
+          swiperImg: that.data.home.swiperImg
+        })
+        that.setData({
+          article: that.data.home.article
+        })
+        that.displayArticle()
+        console.log(that.data.article)
+        console.log(that.data.articleDisplay)
+      },
+      fail: function() {
+        console.log('fail')
+      },
+      complete: function() {
+        console.log('complete')
+      }
+    })
+  }
 })
