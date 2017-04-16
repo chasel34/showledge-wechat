72c96caecff95567b39e9c9796c563ec6b91605f
chasel <z.t.chasel@gmail.com>
2017-04-16 14:34:37 +0800

    add scrolltodown refresh

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,16 +8,22 @@ Page({
   onLoad: function() {
     this.fetchData()
   },
+  /* 初始化展示列表和下拉时加载数据 */
   displayArticle: function() {
     let that = this
     let length = this.data.articleDisplay.length
-    for(let i = length; i < length + 20; i++) {
-      this.setData({
-        'articleDisplay[1]': this.data.article[i] 
-      })
+    let count = 0
+    if((length + 20) < this.data.article.length) {
+      count = length + 20
+    } else {
+      count = this.data.article.length
     }
-    console.log(param)
-    console.log(this.data.param)
+    for(let i = length; i < count; i++) {
+      let param = {}
+      let string = 'articleDisplay[' + i + ']'
+      param[string] = that.data.article[i]
+      this.setData(param)
+    }
   },
   fetchData: function() {
     let that = this
@@ -34,14 +40,9 @@ Page({
           article: that.data.home.article
         })
         that.displayArticle()
-        console.log(that.data.article)
-        console.log(that.data.articleDisplay)
       },
       fail: function() {
         console.log('fail')
-      },
-      complete: function() {
-        console.log('complete')
       }
     })
   }
